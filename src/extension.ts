f4f0630d50e41765a1c8358456a585a6f4c6bf84
wojun.li <wojun.li@shopee.com>
2022-07-29 16:36:41 +0800

    feat: extension done

--- src/extension.ts
+++ src/extension.ts
@@ -1,25 +1,23 @@
 import * as vscode from 'vscode';
-import transform from 'transform-atomic-css';
+import * as atomic from 'transform-atomic-css';
 
 export function activate(context: vscode.ExtensionContext) {
 	
 	console.log('Congratulations, your extension "vscode-atomize-css" is now active!');
 
-	const disposable = vscode.commands.registerCommand('extension.atomize', () => {
+	const disposable = vscode.commands.registerCommand('extension.atomize', async () => {
 		vscode.window.showInformationMessage('Wellcome to use Atomize CSS!');
     const document = vscode.window.activeTextEditor?.document;
-    const text = document?.getText();
+    const text: string = document?.getText() || '';
     console.log(text);
 
-    const transformText = transform(test);
+    const transformText: string = await atomic.transformer(text);
 
     vscode.window.activeTextEditor?.edit(editBuilder => {
       // from begin to end position, replace all 
       const end = new vscode.Position((document?.lineCount || 0) + 1, 0);
-      const text = transformText;
-      editBuilder.replace(new vscode.Range(new vscode.Position(0, 0), end), text);
+      editBuilder.replace(new vscode.Range(new vscode.Position(0, 0), end), transformText);
     });
-    
 	});
 
 	context.subscriptions.push(disposable);
